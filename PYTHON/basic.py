41702f549ba3ea9ca609a52899a1716e878f236d
Ajaychaudhary9670 <ajajaychaudhary1616@gmail.com>
2025-10-31 16:16:17 +0530

    practical lab 1

--- PYTHON/basic.py
+++ PYTHON/basic.py
@@ -1,4 +1,71 @@
-x=4
-y=7
-z=x+y
-print(z)
+# print ("hello world")
+# print("ajay chaudhary")
+# print("hello!","ajay chaudhary")
+# print(28,38,38)
+# print(38+39-3)
+# name = "ajay chaudhary"
+# age = 22
+# price = 2233
+# print(name)
+# print(price)
+# print(age)
+# print("My name is :",name)
+
+# a = 3
+# b = 348
+# sum = a*b
+# print(sum)
+
+# a =4
+# a-=5
+# print(a)
+
+# val = True
+# val1 = False
+# print(val and val1)
+# print(val or val1)
+# a = 20
+# b = 10
+# print(a==b and a>b)
+# type conversion
+# a = int("5")
+# b = 3
+ 
+# print(a + b)
+
+ # input taking
+
+# name  = input("enter your name : ")
+# print("welcom",name)
+
+# a = int(input("enter valve of a:"))
+# b  = int(input("enter valve of b:"))
+
+# print(a+b)
+# a = int(input("enter side:"))
+# print(a*a)
+# a = int(input("enter first number:"))
+# b = int(input("enter second number:"))
+# print(a>=b)
+
+# marks = int(input("enter the marks:"))
+# if(marks>=90):
+#     grade = "A"
+# elif(marks>=80 and marks<90):    
+#     grade = "B"
+# elif(marks>=70 and marks<80):    
+#     grade = "c"
+# else:
+#     grade = "D"    
+
+# print("grade of student is= ",grade)    
+
+# print("ajay",end =" ")
+# print("chaudhary")
+
+
+# str  = "ajaychaudhary"
+# print(str[1:4])
+
+print("hello");print("bye");print("djjsdjdwij");
+
